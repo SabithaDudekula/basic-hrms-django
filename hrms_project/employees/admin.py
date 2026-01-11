1c2eb231128f13a3ea28841621ee10d12b3a61c7
SabithaDudekulagit config --global user.email dudekulasabitha@gmail.com <dudekulasabitha@gmail.com>
2026-01-11 19:52:21 +0530

    Register Attendance model in admin panel

--- hrms_project/employees/admin.py
+++ hrms_project/employees/admin.py
@@ -1,16 +1,13 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Employee, Attendance
 
+# Register Employee model
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    """
-    Admin configuration for Employee model
-    """
-    list_display = (
-        'name',
-        'email',
-        'address',
-        'department',
-        'designation',
-        'date_of_joining',
-    )
+    list_display = ("id", "name", "department", "designation", "email")
+
+
+# Register Attendance model
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ("employee", "date", "in_time", "out_time")
